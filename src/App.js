68f125d6e6c60a247c60453ce9360b65697f9a43
Manuel Micu <manu4216@gmail.com>
2017-08-15 11:24:44 +0200

    Enable Hot Module Reloading

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import "./App.css";
 
 class App extends Component {
     render() {
+        const helloWorld = "Hello World!!!";
+
         return (
             <div className="App">
                 <div className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
-                    <h2>Welcome to React</h2>
+                    <h2>
+                        {helloWorld}
+                    </h2>
                 </div>
                 <p className="App-intro">
                     To get started, edit <code>src/App.js</code> and save to
