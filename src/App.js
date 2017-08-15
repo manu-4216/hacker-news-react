1c389441ec73ab01d3870bae96bd9dcef0f649fe
Manuel Micu <manu4216@gmail.com>
2017-08-15 17:59:06 +0200

    Add static list and filter

--- src/App.js
+++ src/App.js
@@ -2,24 +2,94 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-class App extends Component {
-    render() {
-        const helloWorld = "Hello World!!!";
+const list = [
+    {
+        title: "React",
+        url: "https://facebook.github.io/react/",
+        author: "Jordan Walke",
+        num_comments: 3,
+        points: 4,
+        objectID: 0
+    },
+    {
+        title: "Redux",
+        url: "https://github.com/reactjs/redux",
+        author: "Dan Abramov, Andrew Clark",
+        num_comments: 2,
+        points: 5,
+        objectID: 1
+    }
+];
 
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            list,
+            searchTerm: ""
+        };
+
+        this.onDismiss = this.onDismiss.bind(this);
+        this.onSearchChange = this.onSearchChange.bind(this);
+    }
+
+    render() {
+        const { searchTerm, list } = this.state;
         return (
             <div className="App">
-                <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h2>
-                        {helloWorld}
-                    </h2>
-                </div>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to
-                    reload
-                </p>
+                <form>
+                    <input
+                        type="text"
+                        value={searchTerm}
+                        onChange={this.onSearchChange}
+                    />
+                </form>
+
+                {list
+                    .filter(item =>
+                        item.title
+                            .toLowerCase()
+                            .includes(searchTerm.toLowerCase())
+                    )
+                    .map(item =>
+                        <div key={item.objectID}>
+                            <span>
+                                <a href={item.url}>
+                                    {item.title}
+                                </a>
+                            </span>
+                            <span>
+                                {item.author}
+                            </span>
+                            <span>
+                                {item.num_comments}
+                            </span>
+                            <span>
+                                {item.points}
+                            </span>
+                            <span>
+                                <button
+                                    onClick={() =>
+                                        this.onDismiss(item.objectID)}
+                                    type="button"
+                                >
+                                    Dismiss
+                                </button>
+                            </span>
+                        </div>
+                    )}
             </div>
         );
+    }
+
+    onDismiss(id) {
+        const isNotId = item => item.objectID !== id;
+        const updatedList = this.state.list.filter(isNotId);
+        this.setState({ list: updatedList });
+    }
+
+    onSearchChange(event) {
+        this.setState({ searchTerm: event.target.value });
     }
 }
 
