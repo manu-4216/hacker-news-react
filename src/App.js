e32b829c09f026e26f12f110060c21e44928039d
Manuel Micu <manu4216@gmail.com>
2017-08-17 01:52:46 +0200

    Add basic CSS styling

--- src/App.js
+++ src/App.js
@@ -35,10 +35,12 @@ class App extends Component {
     render() {
         const { searchTerm, list } = this.state;
         return (
-            <div className="App">
-                <Search value={searchTerm} onChange={this.onSearchChange}>
-                    Search
-                </Search>
+            <div className="page">
+                <div className="interactions">
+                    <Search value={searchTerm} onChange={this.onSearchChange}>
+                        Search
+                    </Search>
+                </div>
 
                 <Table
                     list={list}
@@ -69,29 +71,32 @@ const Search = ({ value, onChange, children }) =>
     </form>;
 
 const Table = ({ list, pattern, onDismiss }) =>
-    <div>
+    <div className="table">
         {list
             .filter(item =>
                 item.title.toLowerCase().includes(pattern.toLowerCase())
             )
             .map(item =>
-                <div key={item.objectID}>
-                    <span>
+                <div key={item.objectID} className="table-row">
+                    <span style={{ width: "40%" }}>
                         <a href={item.url}>
                             {item.title}
                         </a>
                     </span>
-                    <span>
+                    <span style={{ width: "30%" }}>
                         {item.author}
                     </span>
-                    <span>
+                    <span style={{ width: "10%" }}>
                         {item.num_comments}
                     </span>
-                    <span>
+                    <span style={{ width: "10%" }}>
                         {item.points}
                     </span>
-                    <span>
-                        <Button onClick={() => onDismiss(item.objectID)}>
+                    <span style={{ width: "10%" }}>
+                        <Button
+                            onClick={() => onDismiss(item.objectID)}
+                            className="button-inline"
+                        >
                             Dismiss
                         </Button>
                     </span>
