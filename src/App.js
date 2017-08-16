2901b2f522fd9b4365c100467f53c46b6dfc5b9e
Manuel Micu <manu4216@gmail.com>
2017-08-16 22:03:54 +0200

    Refactor the Func Stateless Comp into ES6 arrow fct

--- src/App.js
+++ src/App.js
@@ -62,53 +62,44 @@ class App extends Component {
 
 export default App;
 
-function Search({ value, onChange, children }) {
-    return (
-        <form>
-            {children}
-            <input type="text" value={value} onChange={onChange} />
-        </form>
-    );
-}
+const Search = ({ value, onChange, children }) =>
+    <form>
+        {children}
+        <input type="text" value={value} onChange={onChange} />
+    </form>;
 
-function Table({ list, pattern, onDismiss }) {
-    return (
-        <div>
-            {list
-                .filter(item =>
-                    item.title.toLowerCase().includes(pattern.toLowerCase())
-                )
-                .map(item =>
-                    <div key={item.objectID}>
-                        <span>
-                            <a href={item.url}>
-                                {item.title}
-                            </a>
-                        </span>
-                        <span>
-                            {item.author}
-                        </span>
-                        <span>
-                            {item.num_comments}
-                        </span>
-                        <span>
-                            {item.points}
-                        </span>
-                        <span>
-                            <Button onClick={() => onDismiss(item.objectID)}>
-                                Dismiss
-                            </Button>
-                        </span>
-                    </div>
-                )}
-        </div>
-    );
-}
+const Table = ({ list, pattern, onDismiss }) =>
+    <div>
+        {list
+            .filter(item =>
+                item.title.toLowerCase().includes(pattern.toLowerCase())
+            )
+            .map(item =>
+                <div key={item.objectID}>
+                    <span>
+                        <a href={item.url}>
+                            {item.title}
+                        </a>
+                    </span>
+                    <span>
+                        {item.author}
+                    </span>
+                    <span>
+                        {item.num_comments}
+                    </span>
+                    <span>
+                        {item.points}
+                    </span>
+                    <span>
+                        <Button onClick={() => onDismiss(item.objectID)}>
+                            Dismiss
+                        </Button>
+                    </span>
+                </div>
+            )}
+    </div>;
 
-function Button({ onClick, className = "", children }) {
-    return (
-        <button className={className} onClick={onClick} type="button">
-            {children}
-        </button>
-    );
-}
+const Button = ({ onClick, className = "", children }) =>
+    <button className={className} onClick={onClick} type="button">
+        {children}
+    </button>;
