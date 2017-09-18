7494cfbc587e38934dcbd64f756c3621a6e22177
Manuel Micu <manu4216@gmail.com>
2017-09-18 21:31:20 +0200

    Add HOC withLoading

--- src/App.js
+++ src/App.js
@@ -87,17 +87,13 @@ class App extends Component {
                 <Table list={list} onDismiss={this.onDismiss} />
 
                 <div className="interactions">
-                    {isLoading
-                        ? <Loading />
-                        : <Button
-                              onClick={() =>
-                                  this.fetchSearchTopstories(
-                                      searchKey,
-                                      page + 1
-                                  )}
-                          >
-                              More
-                          </Button>}
+                    <ButtonWithLoading
+                        isLoading={isLoading}
+                        onClick={() =>
+                            this.fetchSearchTopstories(searchKey, page + 1)}
+                    >
+                        More
+                    </ButtonWithLoading>
                 </div>
             </div>
         );
@@ -209,5 +205,10 @@ Button.propTypes = {
 };
 
 const Loading = () => <div>Loading ...</div>;
+
+const withLoading = Component => ({ isLoading, ...rest }) =>
+    isLoading ? <Loading /> : <Component {...rest} />;
+
+const ButtonWithLoading = withLoading(Button);
 
 export { Button, Search, Table };
